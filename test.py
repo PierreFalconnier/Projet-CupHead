a5bee55e89b74fa5afa4c2bb31e0599b56c14551
Falconnier <pierrefalco@hotmail.fr>
2022-12-01 01:30:29 +0100

    maj test

--- test.py
+++ test.py
@@ -54,38 +54,66 @@ if __name__=='__main__':
 
     if os.name == 'nt':
         from mss.windows import MSS as mss
-        import win32gui
-        print(gw.getAllTitles()) # nom de la fenêtre = 'Cuphead'
-        print(gw.getWindowsWithTitle('Cuphead'))
-        window = gw.getWindowsWithTitle('Cuphead')[-1]
-        window.restore()
+        # import win32gui
+        # print(gw.getAllTitles()) # nom de la fenêtre = 'Cuphead'
+        # print(gw.getWindowsWithTitle('Cuphead'))
+        # window = gw.getWindowsWithTitle('Cuphead')[-1]
+        # window.restore()
     else:
         from mss.linux import MSS as mss
-
 
     step = 20000
     proba = 0.5
     rate = 0.999965
-
     print("STEPS needed to reach proba : ",math.log(proba)/math.log(rate))
     print("Rate to reach proba after given step : ", proba**(1/step))
 
-    with mss() as sct:
-        im = sct.grab(sct.monitors[1])
-        # rgb = numpy_flip(im)
-        rgb = screenshot_process(im)
-        from torchvision.transforms import ToTensor, Resize, Grayscale, Compose
-        TR_ARRAY2TENSOR = ToTensor()
-        TR_COLOR2GRAY = Grayscale()
-        TR_RESIZE = Resize((128,128))
-        TR_LIST = [TR_ARRAY2TENSOR,TR_COLOR2GRAY]
-        transform = Compose(TR_LIST)
+    mon = {'top': 0, 'left': 0, 'width': 1920//2, 'height': 1080//2} 
 
-        rgb =  transform(rgb)
-        # print(rgb)
-        print(type(rgb))
-        print(rgb.shape)
-        print(rgb)
+    with mss() as sct:
+        bgra_array = np.array(sct.grab(mon)  , dtype=np.uint8)
+        img1 =  np.flip(bgra_array[:, :, :3], 2)
+        img1=cv2.cvtColor(img1, cv2.COLOR_BGRA2GRAY)
+
+        t1 = time.time()
+        cv2.namedWindow("Image",cv2.WINDOW_NORMAL)
+        while True :
+            bgra_array = np.array(sct.grab(mon)  , dtype=np.uint8)
+            img2 =  np.flip(bgra_array[:, :, :3], 2)
+            img2=cv2.cvtColor(img2, cv2.COLOR_BGRA2GRAY)
+
+            res = cv2.matchTemplate(img2,img1, eval('cv2.TM_CCOEFF_NORMED')) 
+            # (res >= 0.8).any()
+   
+            # cv2.imshow("Image", img2)
+            # print(res)
+
+            bgra_array = np.array(sct.grab(mon)  , dtype=np.uint8)
+            img1 =  np.flip(bgra_array[:, :, :3], 2)
+            img1=cv2.cvtColor(img1, cv2.COLOR_BGRA2GRAY)
+
+
+# https://docs.opencv.org/3.4/d4/dee/tutorial_optical_flow.html
+# https://youtu.be/hfXMw2dQO4E
+
+            # cv2.calcOpticalFlowPyrLK(img2, img1, )
+
+
+            # print("FPS : ", int(1/(time.time()-t1)))
+            t1 = time.time()
+    
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    
+    
+cv2.destroyAllWindows()
+        
+
+
+    
+
+
+        
 
         
 
