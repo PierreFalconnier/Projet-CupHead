65d84e5e29f44884710e61935a6f996d6b1a9472
Falconnier Pierre <pierrefalco@hotmail.fr>
2022-12-05 16:27:30 +0100

    ajout optical flow

---
+++ optical_test.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+'''
+example to show optical flow
+
+USAGE: opt_flow.py [<video_source>]
+
+Keys:
+ 1 - toggle HSV flow visualization
+ 2 - toggle glitch
+
+Keys:
+    ESC    - exit
+'''
+
+# Python 2/3 compatibility
+from __future__ import print_function
+
+import numpy as np
+import cv2 as cv
+
+
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv.resize(image, dim, interpolation=inter)
+
+
+def draw_flow(img, flow, step=16):
+    h, w = img.shape[:2]
+    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
+    fx, fy = flow[y,x].T
+    lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+    vis = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+    cv.polylines(vis, lines, 0, (0, 255, 0))
+    for (x1, y1), (_x2, _y2) in lines:
+        cv.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    return vis
+
+
+def draw_hsv(flow):
+    h, w = flow.shape[:2]
+    fx, fy = flow[:,:,0], flow[:,:,1]
+    ang = np.arctan2(fy, fx) + np.pi
+    v = np.sqrt(fx*fx+fy*fy)
+    hsv = np.zeros((h, w, 3), np.uint8)
+    hsv[...,0] = ang*(180/np.pi/2)
+    hsv[...,1] = 255
+    hsv[...,2] = np.minimum(v*4, 255)
+    bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+    return bgr
+
+
+def warp_flow(img, flow):
+    h, w = flow.shape[:2]
+    flow = -flow
+    flow[:,:,0] += np.arange(w)
+    flow[:,:,1] += np.arange(h)[:,np.newaxis]
+    res = cv.remap(img, flow, None, cv.INTER_LINEAR)
+    return res
+
+def main(use_cam = True):
+    import os
+    if os.name == 'nt':
+        import pygetwindow as gw
+        from mss.windows import MSS as mss
+    else:
+        from mss.linux import MSS as mss
+    mon = {'top': 0, 'left': 0, 'width': 1920//2, 'height': 1080//2} 
+
+    import sys
+    import cv2
+    try:
+        fn = sys.argv[1]
+    except IndexError:
+        fn = 0
+
+    with mss() as sct:
+        if use_cam:
+            cam = cv2.VideoCapture(0)
+            _ret, prev = cam.read()
+            prevgray = cv.cvtColor(prev, cv.COLOR_BGR2GRAY)
+        else :
+            bgra_array = np.array(sct.grab(mon)  , dtype=np.uint8)
+            prev =  np.flip(bgra_array[:, :, :3], 2)
+            prevgray=cv2.cvtColor(prev, cv2.COLOR_BGRA2GRAY)
+
+        show_hsv = False
+        show_glitch = False
+        cur_glitch = prev.copy()
+
+
+        while True:
+            if use_cam:
+                _ret, img = cam.read()
+                gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            else:
+                bgra_array = np.array(sct.grab(mon)  , dtype=np.uint8)
+                img =  np.flip(bgra_array[:, :, :3], 2)
+                gray=cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+
+            flow = cv.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            prevgray = gray
+
+          
+            cv.imshow('flow', ResizeWithAspectRatio(draw_flow(gray, flow), width=800))
+            if show_hsv:
+                cv.imshow('flow HSV', draw_hsv(flow))
+            if show_glitch:
+                cur_glitch = warp_flow(cur_glitch, flow)
+                cv.imshow('glitch', cur_glitch)
+
+            ch = cv.waitKey(5)
+            if ch == 27:
+                break
+            if ch == ord('1'):
+                show_hsv = not show_hsv
+                print('HSV flow visualization is', ['off', 'on'][show_hsv])
+            if ch == ord('2'):
+                show_glitch = not show_glitch
+                if show_glitch:
+                    cur_glitch = img.copy()
+                print('glitch is', ['off', 'on'][show_glitch])
+
+        print('Done')
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    main(use_cam=True)
+    cv.destroyAllWindows()
--- test.py
+++ test.py
@@ -16,7 +16,6 @@ from subprocess import check_output
 import re
 import math
 import os
-import pygetwindow as gw
 import numpy
 from PIL import Image
 
@@ -24,6 +23,7 @@ from PIL import Image
 if __name__=='__main__':
 
     if os.name == 'nt':
+        import pygetwindow as gw
         from mss.windows import MSS as mss
         # import win32gui
         # print(gw.getAllTitles()) # nom de la fenêtre = 'Cuphead'
@@ -39,7 +39,19 @@ if __name__=='__main__':
     print("STEPS needed to reach proba : ",math.log(proba)/math.log(rate))
     print("Rate to reach proba after given step : ", proba**(1/step))
 
-    mon = {'top': 0, 'left': 0, 'width': 1920//2, 'height': 1080//2} 
+    # mon = {'top': 0, 'left': 0, 'width': 1920//2, 'height': 1080//2} 
+    mon = {'top': 1080//4, 'left': 0, 'width': 1920//2, 'height': 1080//2} 
+
+
+    # params for ShiTomasi corner detection
+    feature_params = dict( maxCorners = 100,
+                        qualityLevel = 0.3,
+                        minDistance = 7,
+                        blockSize = 7 )
+    # Parameters for lucas kanade optical flow
+    lk_params = dict( winSize  = (15, 15),
+                    maxLevel = 2,
+                    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
     with mss() as sct:
         bgra_array = np.array(sct.grab(mon)  , dtype=np.uint8)
@@ -48,26 +60,80 @@ if __name__=='__main__':
 
         t1 = time.time()
         cv2.namedWindow("Image",cv2.WINDOW_NORMAL)
+
+
+
+
+
+        # params for ShiTomasi corner detection
+        feature_params = dict( maxCorners = 100,
+                            qualityLevel = 0.3,
+                            minDistance = 7,
+                            blockSize = 7 )
+        # Parameters for lucas kanade optical flow
+        lk_params = dict( winSize  = (15, 15),
+                        maxLevel = 2,
+                        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        # Create some random colors
+        color = np.random.randint(0, 255, (100, 3))
+        # Take first frame and find corners in it
+        old_frame = img1
+        old_gray = old_frame
+        p0 = cv2.goodFeaturesToTrack(old_frame, mask = None, **feature_params)
+        # Create a mask image for drawing purposes
+        mask = np.zeros_like(old_frame)
+
         while True :
             bgra_array = np.array(sct.grab(mon)  , dtype=np.uint8)
             img2 =  np.flip(bgra_array[:, :, :3], 2)
             img2=cv2.cvtColor(img2, cv2.COLOR_BGRA2GRAY)
+            frame = img2
 
-            res = cv2.matchTemplate(img2,img1, eval('cv2.TM_CCOEFF_NORMED')) 
+
+             # calculate optical flow
+            p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame, p0, None, **lk_params)
+            # Select good points
+            if p1 is not None:
+                good_new = p1[st==1]
+                good_old = p0[st==1]
+            # draw the tracks
+            for i, (new, old) in enumerate(zip(good_new, good_old)):
+                a, b = new.ravel()
+                c, d = old.ravel()
+                mask = cv2.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
+                frame = cv2.circle(frame, (int(a), int(b)), 5, color[i].tolist(), -1)
+            img = cv2.add(frame, mask)
+            cv2.imshow('frame', img)
+
+
+            p0 = good_new.reshape(-1, 1, 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            # res = cv2.matchTemplate(frame,old_gray, eval('cv2.TM_CCOEFF_NORMED')) 
             # (res >= 0.8).any()
    
             # cv2.imshow("Image", img2)
             # print(res)
 
-            bgra_array = np.array(sct.grab(mon)  , dtype=np.uint8)
-            img1 =  np.flip(bgra_array[:, :, :3], 2)
-            img1=cv2.cvtColor(img1, cv2.COLOR_BGRA2GRAY)
+            # # https://docs.opencv.org/3.4/d4/dee/tutorial_optical_flow.html
+            # # https://youtu.be/hfXMw2dQO4E
 
 
-            # https://docs.opencv.org/3.4/d4/dee/tutorial_optical_flow.html
-            # https://youtu.be/hfXMw2dQO4E
-
-            # cv2.calcOpticalFlowPyrLK(img2, img1, )
+            old_gray = frame.copy()
 
 
             # print("FPS : ", int(1/(time.time()-t1)))
